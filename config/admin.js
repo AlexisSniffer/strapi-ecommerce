931e35c91092d0ebf62b9c89f8bcac4a033eecb1
Alexis Sniffer <alexis.sniffer@gmail.com>
2022-06-08 09:29:51 -0500

    API_TOKEN_SALT

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '4047ae555fee92725496448cbba4c14e'),
   },
-});
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
+})
